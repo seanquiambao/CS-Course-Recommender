d7bc496e6593bb3ac8a3837097b2af2599a6807f
ZacharyTa <Zachary.C.Ta@gmail.com>
2022-11-07 18:19:00 -0800

    Added SQL table testing feature

--- include/SQL.h
+++ include/SQL.h
@@ -1,5 +1,6 @@
 // SQL.h
 #include <string>
+#include <vector>
 #include <sqlite3.h> 
 
 using namespace std;
@@ -13,6 +14,18 @@ class SQL {
         sqlite3 *db;
         char *zErrMsg;
         int rc;
+ 
+        //2D Vector thats stores info for SQL Table Columns (column name, data type, condition)
+        vector< vector<string> > coursesDifficultyOutline = {
+            {"name", "TEXT", "PRIMARY KEY", "NOT NULL"},
+            {"difficulty", "INT", "NOT NULL"}
+        };
+
+        // create type of SQL table in database based on passed in tableName
+        void _createTable(string tableName);
+
+        // Insert data into test table
+        void _insertTestTable(string name, string age);
 
         //_easyClass("SOC-A") -> "ECON 003"
         string _easyClass(string requirementName);
@@ -26,6 +39,13 @@ class SQL {
 
         // Destructor
         ~SQL();
+
+        //Getters
+        void createTable(string tableName) {_createTable(tableName);}
+        void insertTestTable(string name, string age) {_insertTestTable(name, age);}
+
+        // Prints out the data entries of test table
+        void printTestTable();
 
         //getEasiestClass
         string getEasiestCourse(string requirementName, int limit = 3);
