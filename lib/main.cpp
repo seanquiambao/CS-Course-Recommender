9d78cfc3e18e093939a18ab2bbccbd0fbd4c1d6b
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2022-11-22 12:39:47 -0800

    Edited main file

--- lib/main.cpp
+++ lib/main.cpp
@@ -7,29 +7,13 @@
 using namespace std;
 
 int main() {
+    SQL * database = new SQL();
+    loginSystem login;
     CourseRecommender* courseR = new CourseRecommender();
     courseR->printEasiestClasses();
-    // cout << "Compiled YES" << endl;
-    // SQL* database = new SQL();
-
-    // cout << "Creating tables..." << endl;
-    // database->_createTable("Breadth Courses");
-    // database->_createTable("Course Difficulty");
-    // cout << "Reading Data..." << endl;
-    // database->readData("../db/breadthCourses.csv", "Breadth Courses");
-    // database->readData("../db/difficultyDatabase.csv", "Course Difficulty");
-
-    // vector<string> classes = database->_easyClass("HUM-B", 10);
-
-    // for(int i = 0; i < classes.size(); ++i)
-    // {
-    //     cout << classes[i] << " ";
-    // }
-    // cout << endl;
-    // for(int i = 0; i < classes.size(); ++i)
-    // {
-    //     cout << database->_getValue("difficulty", "name", classes[i], "Course Difficulty") << " ";
-    // }
+    database->_createTable("User Database");
+    
+    login.loginPrompt(database);
 
     
     return 0;
