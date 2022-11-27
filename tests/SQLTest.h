062e6b2d2a7342b8ca07aa65bc2aa8b85b81ea2f
toastmeal <slquiambao03@gmail.com>
2022-11-27 14:20:29 -0800

    Added Test Templates

---
+++ tests/SQLTest.h
@@ -0,0 +1,12 @@
+#ifndef __SQLTEST_H__
+#define __SQLTEST_H__
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../include/SQL.h"
+
+
+TEST(SQLTest, TestCheck) {
+    EXPECT_TRUE(true);
+}
+
+#endif
